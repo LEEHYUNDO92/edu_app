58a601d9aa44305a0dd2ff1767d672e97c2b0400
LEEHYUNDO <doleedolee92@gmail.com>
2023-02-06 20:49:36 +0900

    2023-02-06 20:50

--- 클라이언트.py
+++ 클라이언트.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from socket import *
 from threading import *
 
@@ -8,6 +9,7 @@ from PyQt5 import uic, QtGui
 
 from os import environ
 
+import json
 
 form_class = uic.loadUiType("edu.ui")[0]
 
@@ -24,15 +26,62 @@ class Main(QMainWindow, form_class):
 
         self.teacher_send_btn.clicked.connect(self.teacher_chat_send)
         self.student_send_btn.clicked.connect(self.student_chat_send)
+        self.teacher_quiz_send_btn.clicked.connect(self.teacher_quiz_send)
+        self.teacher_quiz_title_list.itemClicked.connect(self.click_topic)
+
+        self.show_teacher_topic()
+        self.show_teacher_score()
+
+        self.a_btn.clicked.connect(self.show_teacher_topic)
+
+    def show_teacher_score(self):
+        time.sleep(0.5)
+        data = {'method': '005'}
+        print(data)
+        json_data = json.dumps(data)
+        self.client_socket.sendall(json_data.encode())
+
+
+    def click_topic(self):
+        self.teacher_quiz_list.clear()
+        text = self.teacher_quiz_title_list.selectedItems()
+        topic = text[0].text()
+        data = {'method': '004', 'text': topic}
+        json_data = json.dumps(data)
+        self.client_socket.sendall(json_data.encode())
+
+    def show_teacher_topic(self):
+        data = {'method': '003'}
+        json_data = json.dumps(data)
+        self.client_socket.sendall(json_data.encode())
+
+    def teacher_quiz_send(self):
+        quiz = []
+        quiz_list = [self.teacher_quiz_title_edit.text(), self.teacher_quiz_edit1.text(), self.teacher_quiz_edit2.text(), self.teacher_quiz_edit3.text(), self.teacher_quiz_edit4.text(), self.teacher_quiz_edit5.text(), self.teacher_quiz_answer_edit1.text(), self.teacher_quiz_answer_edit2.text(), self.teacher_quiz_answer_edit3.text(), self.teacher_quiz_answer_edit4.text(), self.teacher_quiz_answer_edit5.text()]
+
+        for x in quiz_list:
+            quiz.append(x)
+            print(quiz)
+        data = {'method': '002', 'text': quiz}
+        json_data = json.dumps(data)
+        self.client_socket.sendall(json_data.encode())
 
     def student_chat_send(self):
         text = self.student_text_edit.text()
-        self.client_socket.send((text + "001").encode('utf-8'))
+
+        data = {'method': '001', 'text': text}
+        json_data = json.dumps(data)
+        self.client_socket.sendall(json_data.encode())
+
         self.student_text_edit.clear()
 
     def teacher_chat_send(self):
         text = self.teacher_text_edit.text()
-        self.client_socket.send((text+"001").encode('utf-8'))
+
+        data = {'method': '001', 'text': text}
+        json_data = json.dumps(data)
+        self.client_socket.sendall(json_data.encode())
+
         self.teacher_text_edit.clear()
 
     def btn11(self):
@@ -53,15 +102,45 @@ class Main(QMainWindow, form_class):
 
     def receive_message(self,so):
         while True:
-            buf = so.recv(512)
-            text = buf.decode('utf-8')
-            if not buf: #연결 종료 됨
+            try:
+                data = self.client_socket.recv(9999)
+                dic_data = json.loads(data.decode())
+            except:
                 break
-            if text[-3:] == '001':
-                self.teacher_chat_view_list.addItem(text[:-3])
-                self.student_chat_view_list.addItem(text[:-3])
-            elif text[-3:] == '002':
-                print("b")
+            else:
+                # if not buf: #연결 종료 됨
+                #     break
+                if dic_data['method'] == '001':
+                    print(dic_data['text'])
+                    self.teacher_chat_view_list.addItem(dic_data['text'])
+                    self.student_chat_view_list.addItem(dic_data['text'])
+                elif dic_data['method'] == '002':
+                    print("b")
+                elif dic_data['method'] == '003':
+                    print(dic_data)
+                    print(dic_data['text'])
+                    for x in dic_data['text']:
+                        self.teacher_quiz_title_list.addItem(x[0])
+                elif dic_data['method'] == '004':
+                    for x in dic_data['text']:
+                        self.teacher_quiz_list.addItem(x[0])
+                elif dic_data['method'] == '005':
+                    print(dic_data)
+                    print(dic_data['text'])
+                    print(len(dic_data['text']))
+                    self.score_table.setRowCount(len(dic_data['text']))
+                    for i in range(len(dic_data['text'])):
+                        self.score_table.setItem(i, 0, QTableWidgetItem(dic_data['text'][i][0]))
+                        self.score_table.setItem(i, 1, QTableWidgetItem(str(dic_data['text'][i][1])))
+                        self.score_table.setItem(i, 2, QTableWidgetItem(dic_data['text'][i][2]))
+                        self.score_table.setItem(i, 3, QTableWidgetItem(dic_data['text'][i][3]))
+                        self.score_table.setItem(i, 4, QTableWidgetItem(dic_data['text'][i][4]))
+                        self.score_table.setItem(i, 5, QTableWidgetItem(dic_data['text'][i][5]))
+                        self.score_table.setItem(i, 6, QTableWidgetItem(dic_data['text'][i][6]))
+                        self.score_table.setItem(i, 7, QTableWidgetItem(dic_data['text'][i][7]))
+                        self.score_table.setItem(i, 8, QTableWidgetItem(str(dic_data['text'][i][8])))
+                    # for x in dic_data['text']:
+                    #     print(x)
         so.close()
 
 
